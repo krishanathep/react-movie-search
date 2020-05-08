8f003daeaa5bcf42f6b42dd0b53887e454cce163
krishanathep <krishanathep@gmail.com>
2020-05-08 16:42:45 +0700

    Updates

---
+++ precache-manifest.0b2f9217ed2796545424c8143445d30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc5a81ae51933adce3e98262fcd884d",
+    "url": "/react-movie-search/index.html"
+  },
+  {
+    "revision": "c32e62d053055f9139c8",
+    "url": "/react-movie-search/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d800c23519c30de34a15",
+    "url": "/react-movie-search/static/js/2.8fe0158a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-search/static/js/2.8fe0158a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32e62d053055f9139c8",
+    "url": "/react-movie-search/static/js/main.41d25117.chunk.js"
+  },
+  {
+    "revision": "5ab8775d1255e066614c",
+    "url": "/react-movie-search/static/js/runtime-main.256cb43b.js"
+  }
+]);
