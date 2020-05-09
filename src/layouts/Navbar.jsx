ba8a1d9fe6cf6d4238cfc294fa98ec1901b87a51
Krishanathep <krishanathep@gmail.com>
2020-05-09 22:41:37 +0700

    Update Github Link

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -7,9 +7,14 @@ export class Navbar extends Component {
       <div className="Navbar">
         <nav class="navbar navbar-expand-lg bg-primary fixed-top">
           <div className="container">
-          <Link to='/' className="navbar-brand">
-          <i class="fas fa-video"></i>&nbsp; <span>REACT</span> MOVIE SEARCH
-          </Link>
+            <Link to="/" className="navbar-brand">
+              <i class="fas fa-video"></i>&nbsp; <span>REACT</span> MOVIE SEARCH
+            </Link>
+              <ul class="navbar-nav ml-auto">
+                <li className='nav-item'>
+                  <a className='nav-link' href="https://github.com/krishanathep/react-movie-search"><i className="fab fa-github"></i></a>
+                </li>
+              </ul>
           </div>
         </nav>
       </div>
